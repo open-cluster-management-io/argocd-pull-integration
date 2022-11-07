f7c7518bd2bbfe6159fc70d0f94fac3068774b40
Mike Ng <ming@redhat.com>
2022-11-07 09:13:33 -0500

    Initial repo commit
    Signed-off-by: Mike Ng <ming@redhat.com>

---
+++ README.md
@@ -0,0 +1,137 @@
+# ArgoCD Application Pull Controller
+Experimental ArgoCD Application controller that uses the hub-spoke pattern or pull model mechanism for decentralized resource delivery to the remote clusters.
+By using the [Open Cluster Management (OCM)](https://open-cluster-management.io/) APIs and components, 
+the ArgoCD Applications will be pull from the multi-cluster control plane hub cluster down to 
+the registered OCM managed clusters.
+
+**Note:** This is an experimental project and we welcome any community's feedback and suggestions.
+
+## Description
+The current ArgoCD resource delivery is primarily pushing resources from a centralized cluster to the remote/managed clusters.
+
+![push model](assets/push.png)
+
+By using this controller, users can have a pull model resource delivery mechanism.
+
+![pull model](assets/pull.png)
+
+The pull model offers some advantages over the existing push model:
+- Scalability: well documented that hub-spoke pattern offers better scalability.
+- Security: cluster credentials doesn't have to be stored in a centralized environment.
+- Reduce the impact of a single point of centralized failure.
+
+This ArgoCD pull model controller on the Hub cluster will create [ManifestWork](https://open-cluster-management.io/concepts/manifestwork/) objects wrapping Application objects as payload.
+The OCM agent on the Managed cluster will see the ManifestWork on the Hub cluster and pull the Application down.
+
+## Dependencies
+- The Open Cluster Management (OCM) multi-cluster environment needs to be setup. See [OCM website](https://open-cluster-management.io/) on how to setup the environment.
+- In this pull model, OCM will provide the cluster inventory and ability to deliver workload to the remote/managed clusters.
+- Hub cluster and remote/managed clusters need to have ArgoCD Application installed. See [ArgoCD website](https://argo-cd.readthedocs.io/en/stable/getting_started/) for more details.
+
+## Getting Started
+1. Setup an OCM Hub cluster and registered an OCM Managed cluster.
+
+2. Install ArgoCD on both clusters.
+
+3. On the Hub cluster, scale down the Application controller:
+```
+kubectl -n argocd scale statefulset/argocd-application-controller --replicas 0
+```
+
+4. Clone this project and connect to the Hub cluster and start the Pull controller:
+```
+git clone ...
+cd argocd-pull-integration
+export KUBECONFIG=/path/to/<hub-kubeconfig>
+make run
+```
+
+5. If your controller starts successfully, you should see:
+```
+...
+INFO	controller.application	Starting EventSource	{"reconciler group": "argoproj.io", "reconciler kind": "Application", "source": "kind source: *v1alpha1.Application"}
+INFO	controller.application	Starting Controller	{"reconciler group": "argoproj.io", "reconciler kind": "Application"}
+INFO	controller.application	Starting workers	{"reconciler group": "argoproj.io", "reconciler kind": "Application", "worker count": 1}
+...
+```
+
+6. On the Hub cluster, create an ArgoCD cluster secret that represent the managed cluster. This step will be automated in the future.
+**Note** replace the `cluster-name` with the registered managed cluster name.
+```
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: <cluster-name>-secret # cluster1-secret
+  namespace: argocd
+  labels:
+    argocd.argoproj.io/secret-type: cluster
+type: Opaque
+stringData:
+  name: <cluster-name> # cluster1
+  server: https://<cluster-name>-control-plane:6443 # https://cluster1-control-plane:6443
+EOF
+```
+
+7. On the Hub cluster, apply the manifests in `example/hub`:
+```
+kubectl apply -f example/hub
+```
+
+8. On the Managed cluster, apply the manifests in `example/managed`:
+```
+kubectl apply -f example/managed
+```
+
+9. On the Hub cluster, apply the `guestbook-app-set` manifest:
+```
+kubectl apply -f example/guestbook-app-set.yaml
+```
+**Note** The Application template inside the ApplicationSet must contain the following content:
+```
+      labels:
+        argocd.argoproj.io/pull-to-ocm-managed-cluster: 'true'
+      annotations:
+        argocd.argoproj.io/ocm-managed-cluster: '{{name}}'
+```
+The label allows the pull model controller to select the Application for processing.
+The annotation is for the ApplicationSet to generate multiple Application based on each cluster generator targets.
+
+10. When this guestbook ApplicationSet reconciles, it will generate an Application for the registered ManagedCluster. For example:
+```
+$ kubectl -n argocd get appset
+NAME            AGE
+guestbook-app   84s
+$ kubectl -n argocd get app
+NAME                     SYNC STATUS   HEALTH STATUS
+cluster1-guestbook-app     
+```
+
+11. On the Hub cluster, the pull controller will wrap the Application with a ManifestWork. For example:
+```
+$ kubectl -n cluster1 get manifestwork
+NAME                          AGE
+cluster1-guestbook-app-d0e5   2m41s
+```
+
+12. On the Managed cluster, you should see the Application is pulled down successfully. For example:
+```
+$ kubectl -n argocd get app
+NAME                     SYNC STATUS   HEALTH STATUS
+cluster2-guestbook-app   Synced        Healthy
+$ kubectl -n guestbook get deploy
+NAME           READY   UP-TO-DATE   AVAILABLE   AGE
+guestbook-ui   1/1     1            1           7m36s
+```
+
+## Community, discussion, contribution, and support
+
+Check the [CONTRIBUTING Doc](CONTRIBUTING.md) for how to contribute to the repo.
+
+### Communication channels
+
+Slack channel: [#open-cluster-mgmt](https://kubernetes.slack.com/channels/open-cluster-mgmt)
+
+## License
+
+This code is released under the Apache 2.0 license. See the file [LICENSE](LICENSE) for more information.
---
+++ controllers/application/helper.go
@@ -0,0 +1,154 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package application
+
+import (
+	"strconv"
+	"strings"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	argov1alpha1 "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+	appsetv1alpha1 "github.com/argoproj/argo-cd/v2/pkg/apis/applicationset/v1alpha1"
+	workv1 "open-cluster-management.io/api/work/v1"
+)
+
+func containsValidPullLabel(application argov1alpha1.Application) bool {
+	labels := application.GetLabels()
+	if len(labels) == 0 {
+		return false
+	}
+
+	if pullLabelStr, ok := labels[LabelKeyPull]; ok {
+		isPull, err := strconv.ParseBool(pullLabelStr)
+		if err != nil {
+			return false
+		}
+		return isPull
+	}
+
+	return false
+}
+
+func containsValidPullAnnotation(application argov1alpha1.Application) bool {
+	annos := application.GetAnnotations()
+	if len(annos) == 0 {
+		return false
+	}
+
+	managedClusterName, ok := annos[AnnotationKeyOCMManagedCluster]
+	return ok && len(managedClusterName) > 0
+}
+
+// generateAppNamespace returns the intended namespace for the Application in the following priority
+// 1) Annotation specified custom namespace
+// 2) Application's namespace value
+// 3) Fallsback to 'argocd'
+func generateAppNamespace(application argov1alpha1.Application) string {
+	annos := application.GetAnnotations()
+	appNamespace := annos[AnnotationKeyOCMManagedClusterAppNamespace]
+	if len(appNamespace) > 0 {
+		return appNamespace
+	}
+
+	appNamespace = application.GetNamespace()
+	if len(appNamespace) > 0 {
+		return appNamespace
+	}
+
+	return "argocd" // TODO find the constant value from the argo API for this field
+}
+
+// generateManifestWorkName returns the ManifestWork name for a given application.
+// It uses the Application name with the suffix of the first 5 characters of the UID
+func generateManifestWorkName(application argov1alpha1.Application) string {
+	return application.Name + "-" + string(application.UID)[0:5]
+}
+
+// prepareApplicationForWorkPayload modifies the Application:
+// - reset the meta
+// - set the namespace value
+// - ensures the Application Destination is set to in-cluster resource deployment
+func prepareApplicationForWorkPayload(application argov1alpha1.Application) argov1alpha1.Application {
+	application.TypeMeta = metav1.TypeMeta{
+		APIVersion: argov1alpha1.SchemeGroupVersion.String(),
+		Kind:       argov1alpha1.ApplicationSchemaGroupVersionKind.Kind,
+	}
+	application.ObjectMeta = metav1.ObjectMeta{
+		Name:       application.GetName(),
+		Namespace:  generateAppNamespace(application),
+		Finalizers: application.GetFinalizers(),
+	}
+
+	// empty the name
+	application.Spec.Destination.Name = ""
+	// always set for in-cluster destination
+	application.Spec.Destination.Server = argov1alpha1.KubernetesInternalAPIServerAddr
+
+	// empty the status
+	application.Status = argov1alpha1.ApplicationStatus{}
+
+	return application
+}
+
+// generateManifestWork creates the ManifestWork that wraps the Application as payload
+// With the status sync feedback of Application's health status and sync status.
+// The Application payload Spec Destination values are modified so that the Application is always performing in-cluster resource deployments.
+// If the Application is generated from an ApplicationSet, custom label and annotation are inserted.
+func generateManifestWork(name, namespace string, application argov1alpha1.Application) *workv1.ManifestWork {
+	var workLabels map[string]string
+	var workAnnos map[string]string
+	for _, ownerRef := range application.OwnerReferences {
+		if strings.EqualFold(ownerRef.APIVersion, appsetv1alpha1.GroupVersion.String()) &&
+			strings.EqualFold(ownerRef.Kind, "ApplicationSet") { // TODO find the constant value from the argo appset API for this field
+			workLabels = map[string]string{LabelKeyWorkAppSet: strconv.FormatBool(true)}
+			workAnnos = map[string]string{AnnotationKeyWorkAppSet: application.Namespace + "/" + ownerRef.Name}
+		}
+	}
+
+	application = prepareApplicationForWorkPayload(application)
+
+	return &workv1.ManifestWork{ // TODO use OCM API helper to generate manifest work.
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        name,
+			Namespace:   namespace,
+			Labels:      workLabels,
+			Annotations: workAnnos,
+		},
+		Spec: workv1.ManifestWorkSpec{
+			Workload: workv1.ManifestsTemplate{
+				Manifests: []workv1.Manifest{{RawExtension: runtime.RawExtension{Object: &application}}},
+			},
+			ManifestConfigs: []workv1.ManifestConfigOption{
+				{
+					ResourceIdentifier: workv1.ResourceIdentifier{
+						Group:     argov1alpha1.SchemeGroupVersion.Group,
+						Resource:  "applications", // TODO find the constant value from the argo API for this field
+						Namespace: application.Namespace,
+						Name:      application.Name,
+					},
+					FeedbackRules: []workv1.FeedbackRule{
+						{Type: workv1.JSONPathsType, JsonPaths: []workv1.JsonPath{{Name: "healthStatus", Path: ".status.health.status"}}},
+						{Type: workv1.JSONPathsType, JsonPaths: []workv1.JsonPath{{Name: "syncStatus", Path: ".status.sync.status"}}},
+					},
+				},
+			},
+		},
+	}
+}
