cd26894bb43dc063e5be998b4f536cbe637e3a11
Mike Ng <ming@redhat.com>
2026-01-05 11:51:59 -0500

    chore: update golang and dep ver (#152)
    Signed-off-by: Mike Ng <ming@redhat.com>

--- test/e2e/scripts/setup_ocm_env.sh
+++ test/e2e/scripts/setup_ocm_env.sh
@@ -6,6 +6,8 @@ set -o pipefail
 # kind delete clusters --all; kind create cluster --name hub; kind create cluster --name cluster1
 
 KUBECTL=${KUBECTL:-kubectl}
+# clusteradm version - must be set via environment variable (default defined in Makefile)
+CLUSTERADM_VERSION=${CLUSTERADM_VERSION:?CLUSTERADM_VERSION must be set}
 
 BUILD_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 DEPLOY_DIR="$(dirname "$BUILD_DIR")"
@@ -16,17 +18,17 @@ CLUSTERADM="clusteradm"
 
 export PATH=$PATH:${WORK_DIR}/bin
 
-echo "############  Install clusteradm"
-go install open-cluster-management.io/clusteradm/cmd/clusteradm@main
+echo "############  Install clusteradm ${CLUSTERADM_VERSION}"
+go install open-cluster-management.io/clusteradm/cmd/clusteradm@${CLUSTERADM_VERSION}
 
 echo "############ Init hub"
 $KUBECTL config use-context kind-hub
-${CLUSTERADM} init --wait --bundle-version=latest
+${CLUSTERADM} init --wait
 joincmd=$(${CLUSTERADM} get token | grep clusteradm)
 
 echo "############ Init agent as cluster1"
 $KUBECTL config use-context kind-cluster1
-$(echo ${joincmd} --force-internal-endpoint-lookup --wait --bundle-version=latest | sed "s/<cluster_name>/cluster1/g")
+$(echo ${joincmd} --force-internal-endpoint-lookup --wait | sed "s/<cluster_name>/cluster1/g")
 
 echo "############ Accept join of cluster1"
 $KUBECTL config use-context kind-hub
